a21d80940178d032696bbc55cb80c3b2789e4301
pooja-404 <pk3924792@gmail.com>
2024-02-28 22:43:20 +0530

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,26 @@
 function openNav() {
     document.getElementById("navbar").classList.toggle("end-0");
     document.body.classList.toggle("overflow_hidden");
-    document.getElementById("menubtn-icon").classList.toggle("cross");
-
-    // backtotop
-    window.addEventListener('scroll', function () {
-        let backtotop = document.getElementById("backtotop")
-        if (window.scrollY > 300) {
-            backtotop.style.display = 'block'
-        }
-        else {
-            backtotop.style.display = 'none'
-        }
-    });
-    // preloader------
-    setTimeout(() => {
-        document.getElementById("preloader").classList.add("opacity-0");
-        document.getElementById("preloader").classList.add("pointer_event_none")
-        document.body.classList.remove("overflow-hidden")
-    }, 0);
+    document.getElementById("menubtn-icon").classList.toggle("cross")
 
 }
+window.addEventListener('scroll', function () {
+    let backtotop = document.getElementById("backtotop")
+    if (window.scrollY > 300) {
+        backtotop.style.display = 'block'
+    }
+    else {
+        backtotop.style.display = 'none'
+    }
+})
+
+setTimeout(() => {
+    document.getElementById("preloader").classList.add("d-none");
+    document.getElementById("preloader").classList.add("pointer_event_none")
+    document.body.classList.remove("overflow-hidden")
+}
+    , 2000);
+
+
+
+
